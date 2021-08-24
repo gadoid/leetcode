42dc6aa61e6dff750ff8f3a62f7d976d510e7a3b
gadoid <j819902614@live.com>
2021-08-24 23:39:48 +0800

    ,

--- 11 盛最多水的容器/test.java
+++ 11 盛最多水的容器/test.java
@@ -6,8 +6,11 @@ class Solution {
         int len = height.length-1;
         while (low < high){
             int l = height[high]>height[low] ? height[low]:height[high] ;
+            int a1 = height[low];
+            int a2 = height[high];
+            System.out.println(a1+a2);
             area = area > len*l ? area:len*l;
-            if (high>low) low++ ;
+            if (height[high]>height[low]) low++ ;
             else  high-- ;
             len -- ;
         } 
